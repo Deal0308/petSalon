9b39f4fd8c40b0da70caaede3f34c240f3e6da33
Deal0308 <141692520+Deal0308@users.noreply.github.com>
2023-09-09 13:05:51 -0500

    new

--- scripts/register.js
+++ scripts/register.js
@@ -7,35 +7,55 @@ let salon = {
         zip: "123",
         number: "12345"
     },
-    pets:[
-        {
-            name: "Scooby",
-            age: 60,
-            breed: "Labrador Retriever",
-            service:"Grooming"
-        },
-        {
-            name: "Boss",
-            age: 10,
-            breed: "Doberman",
-            service:"Vaccines"
-        },
-        {
-            name: "Speedy",
-            age: 10,
-            breed: "Greyhound",
-            service:"Nail cutting"
-        }
-    ]
+    pets:[]
 }
-function displayPets() {
-    let petNames = '';
-    for (let i = 0; i < pets.length; i++) {
-        petNames += pets[i].name + '<br>';
+//constructor
+function Pet(name,age,breed,service){
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.service = service;
+}   
+//inputs
+const inputName = document.getElementById("txtName");
+const inputAge = document.getElementById("txtAge");
+const inputBreed = document.getElementById("txtBreed");
+const inputService = document.getElementById("txtService");
+function isValid(aPet){
+    let validation = true;//assuming everything is valid
+    if(aPet.value == ""){
+        validation = false;
+        alert("Please enter a name");
     }
-
-    document.getElementById('petNames').innerHTML = petNames;
-    document.getElementById('petCount').innerHTML = 'Number of pets: ' + pets.length;
+    return validation;
 }
+//buttons
 
-window.onload = displayPets;
+function register(){
+    //get the values from the form
+    let newPet = new Pet(inputName.value,inputAge.value,inputBreed.value,inputService.value);
+    if (isValid(newPet)==true){
+        salon.pets.push(newPet);
+        console.log(salon.pets);
+        clearInputs();
+    }
+    
+
+}
+function clearInputs(){
+    inputName.value = "";
+    inputAge.value = "";
+    inputBreed.value = "";
+    inputService.value = "";
+
+
+}
+function init(){
+    let pet1 =new Pet("Scooby",60,"Dane","Grooming");
+    let pet2=new Pet("Buddy",30,"Bulldog","Nail Trimming");
+    let pet3=new Pet("Lucky",20,"Mastiff","Vaccines");
+    
+    salon.pets.push(pet1,pet2,pet3,pet4);//add objects into salon array
+
+}
+window.onload=init;//waiting for the page to load
